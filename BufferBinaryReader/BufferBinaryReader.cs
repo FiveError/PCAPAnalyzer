6ac668ca8c025ace98d6508453cf4012e2376dfc
FiveError <ser9800@mail.ru>
2022-12-01 14:48:40 +0300

    Add class BufferStreamReader and UnitTests

---
+++ BufferBinaryReader/BufferBinaryReader.cs
@@ -0,0 +1,85 @@
+﻿
+namespace BufferStream
+{
+
+    public class BufferBinaryReader : IDisposable
+    {
+        private UInt32 _bufferSize;
+        private byte[] _buffer;
+        private UInt32 _bufferOffset;
+        private Int64 _readDataSize;
+        private Int64 _previousOffset;
+        private Int64 _offset;
+        private string _fileName;
+        private FileStream _stream;
+        private BinaryReader _reader;
+        
+        public void Dispose()
+        {
+            _reader.Close();
+            _stream.Close();
+        }
+        public BufferBinaryReader(string FileName, UInt32 BufferSize = 4096)
+        {
+            _fileName = FileName;
+            if (!IsFileExists())
+            {
+                throw new ArgumentException();
+            }
+            _bufferOffset = 0;
+            _readDataSize = 0;
+            _bufferSize = BufferSize;
+            _buffer = new byte[_bufferSize];
+            _stream = new FileStream(_fileName, FileMode.Open, FileAccess.Read);
+            _reader = new BinaryReader(_stream);
+        }
+        public byte[] ReadBytes(UInt32 Size)
+        {
+            if (_bufferOffset >= _readDataSize)
+            {
+                UpdateBuffer();
+            }
+            if (_bufferOffset + Size > _bufferSize)
+            {
+                UpdateBuffer();
+            }
+            UInt32 readDataSize = (UInt32)Math.Min(_readDataSize - _bufferOffset, Size);
+            byte[] readBytes = new byte[readDataSize];
+            Buffer.BlockCopy(_buffer, (Int32)_bufferOffset, readBytes, 0, (Int32)readDataSize);
+            _bufferOffset += readDataSize;
+            _previousOffset = _offset;
+            _offset += readDataSize;
+            return readBytes;
+
+        }
+        public void Seek(Int64 Offset,SeekOrigin Origin)
+        {
+            _previousOffset = _offset;
+            _offset = _stream.Seek(Offset, Origin);
+
+        }
+        public Int64 GerCurrentOffset()
+        {
+            return _offset;
+        }
+
+        public Int64 GetPreviousOffset()
+        {
+            return _previousOffset;
+        }
+        private void UpdateBuffer()
+        {
+            var remainDataSize = _readDataSize - _bufferOffset;
+            Buffer.BlockCopy(_buffer, (Int32)_bufferOffset, _buffer, 0, (Int32)remainDataSize);
+            _bufferOffset = 0;
+            _readDataSize = _reader.Read(_buffer, (Int32)remainDataSize, (Int32)(_bufferSize - remainDataSize));
+            _readDataSize += remainDataSize;
+
+        }
+        private bool IsFileExists()
+        {
+            return File.Exists(_fileName);
+        }
+
+    }
+}
---
+++ BufferStreamTests/UTBufferBinaryReader.cs
@@ -0,0 +1,104 @@
+using Xunit;
+using BufferStream;
+using System;
+using System.Linq;
+using System.IO;
+
+namespace BufferStreamTests
+{
+    public class UTBufferBinaryReader
+    {
+        [Fact]
+        public void CreatingClass_WrongFileName_ReturnsExecptions()
+        {
+            Assert.Throws<ArgumentException>(
+                () => _ = new BufferBinaryReader(@"BufferBinaryReader\BufferStreamTests\Files\Wrong.pcap")
+                );
+        }
+
+        [Fact]
+        public void Read10Bytes_FileName_RetursByteArray10()
+        {
+            var reader = new BufferBinaryReader(@"Files/TestFile.pcap");
+            byte[] data = reader.ReadBytes(10);
+            Assert.Equal(data.Length, 10);
+        }
+
+        [Fact]
+        public void Read10000Bytes_FileName_RetursBufferSize()
+        {
+            var reader = new BufferBinaryReader(@"Files/TestFile.pcap");
+            byte[] data = reader.ReadBytes(10000);
+            Assert.Equal(data.Length, 4096);
+        }
+        [Theory]
+        [InlineData(@"Files/TestFile.pcap", 1178124)]
+        [InlineData(@"Files/TestFile2.pcap", 6702197)]
+        public void EndOfStream_FileName_ReturnFileSize(string FileName, UInt64 FileSize)
+        {
+            var reader = new BufferBinaryReader(FileName, 40960);
+            UInt64 fileSize = 0;
+            int readSize = reader.ReadBytes(100000).Length;
+            while (readSize > 0)
+            {
+                fileSize += (UInt64)Math.Min(readSize, 100000);
+                readSize = reader.ReadBytes(100000).Length;
+            }
+
+            Assert.Equal(fileSize, (UInt64)FileSize);
+        }
+        [Theory]
+        [InlineData(@"Files/TestFile.pcap", 1178124)]
+        [InlineData(@"Files/TestFile2.pcap", 6702197)]
+        public void ProperlyDataRead_FileName_ReturnsDifferenceCount(string FileName,UInt64 FileSize)
+        {
+            var reader = new BufferBinaryReader(FileName);
+            bool IsEqual = true;
+            using (var stream = new FileStream(FileName, FileMode.Open, FileAccess.Read))
+            {
+                {
+                    using (var readerBinary = new BinaryReader(stream))
+                    {
+                        for (UInt64 current = 0; current <= FileSize; current += 1000)
+                        {
+                            var dataFromActualReader = readerBinary.ReadBytes(1000);
+                            var dataFromExpectedReader = reader.ReadBytes(1000);
+                            IsEqual &= dataFromActualReader.SequenceEqual(dataFromExpectedReader);
+                            //IsEqual &= dataFromActualReader.Equals(dataFromExpectedReader);
+                        }
+                    }
+                }
+            }
+            Assert.Equal(IsEqual, true);
+        }
+       
+        [Theory]
+        [InlineData(@"Files/TestFile.pcap", 1178124)]
+        [InlineData(@"Files/TestFile2.pcap", 6702197)]
+        public void FileSizeUsingSeek_FileName_ReturnsFileSize(string FileName, UInt64 FileSize)
+        {
+            var reader = new BufferBinaryReader(FileName);
+            reader.Seek(0, SeekOrigin.End);
+            var fileSize = reader.GerCurrentOffset();
+            Assert.Equal((long)FileSize, fileSize);
+        }   
+
+        [Fact]
+        public void GetOffsets_FileName_ReturnsOffsets()
+        {
+            var reader = new BufferBinaryReader(@"Files/TestFile.pcap");
+            reader.ReadBytes(1000);
+            Assert.Equal(reader.GerCurrentOffset(), 1000);
+            Assert.Equal(reader.GetPreviousOffset(), 0);
+
+            reader.ReadBytes(1000);
+            Assert.Equal(reader.GerCurrentOffset(), 2000);
+            Assert.Equal(reader.GetPreviousOffset(), 1000);
+
+            reader.Seek(0, SeekOrigin.Begin);
+            Assert.Equal(reader.GerCurrentOffset(), 0);
+            Assert.Equal(reader.GetPreviousOffset(), 2000);
+        }
+
+    }
+}
